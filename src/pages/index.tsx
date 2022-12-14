b80d3eda03fd920995b43159694876163c91a3c8
Lucas Castro <lucasamonrc@gmail.com>
2022-12-14 00:38:45 -0700

    Add home page markup and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <h1>Hello, world</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | Herald</title>
+      </Head>
+
+      <main className="flex items-center justify-between max-w-6xl mx-auto px-8">
+        <section className="max-w-2xl">
+          <span className="text-2xl font-bold">
+            Hark! We bring good tidings to you!
+          </span>
+
+          <h1 className="text-6xl leading-[4.5rem] font-extrabold mt-10">
+            Good news <span className="text-teal-400">delivered</span> to you
+            daily.
+          </h1>
+          <p className="text-2xl mt-6">
+            Get access to latest wholesome stories now <br />
+            <span className="text-teal-400 font-bold">for only $10/month</span>.
+          </p>
+          {/* <SubscribeButton priceId="1234" /> */}
+          <button className="flex mt-10 h-16 w-[260px] rounded bg-teal-400 text-white text-2xl font-bold justify-center items-center transition hover:brightness-75">
+            Subscribe Now
+          </button>
+        </section>
+        <Image src="/images/illustration.svg" alt="" width={400} height={400} />
+      </main>
+    </>
+  );
 }
