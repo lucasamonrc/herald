81f628927cdd9b0f2a104466c5c23f9f7a1f5b0f
Lucas Castro <lucasamonrc@gmail.com>
2022-12-17 21:43:44 -0700

    Implement subscription check and sign up

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
+import supabase from "../../../services/supabase";
+
 export default NextAuth({
   providers: [
     GitHubProvider({
@@ -8,4 +10,35 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      let results = await supabase.from('users').select('*').eq('email', session?.user?.email as string).single();
+
+      if (results.error) {
+        return { ...session, activeSubscription: null }
+      }
+
+      results = await supabase.from('subscription').select('*').eq('user_id', results.data.user_id).single();
+
+      if (results.error) {
+        return { ...session, activeSubscription: null }
+      }
+
+      return {...session, activeSubscription: results.data.active || null };
+    },
+    async signIn({ user, account, profile }) {
+      let results = await supabase.from('users').select('*').eq('email', user.email).single();
+
+      if (results.error) {
+        let { error } = await supabase.from('users').insert({ name: user.name, email: user.email, image: user.image });
+
+        if (error) {
+          console.error(error);
+          return false;
+        };
+      }
+
+      return true;
+    }
+  }
 });
