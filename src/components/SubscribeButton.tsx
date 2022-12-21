576be2698fea665ced3492bcedcd3f53434b859c
Lucas Castro <lucasamonrc@gmail.com>
2022-12-20 19:27:02 -0700

    Fix path to redirect when subscriber tries to sub

--- src/components/SubscribeButton.tsx
+++ src/components/SubscribeButton.tsx
@@ -18,7 +18,7 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     }
 
     if (session.activeSubscription) {
-      router.push("/posts");
+      router.push("/stories");
       return;
     }
 
